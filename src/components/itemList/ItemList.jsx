210eea243941f0728ad417d0f35c5a32f0fa7480
macarena_zamar <mmacarenazamar@gmial.com>
2022-09-01 14:29:48 +0200

    Entrega clase Promises, asingronias, MAP

--- src/components/itemList/ItemList.jsx
+++ src/components/itemList/ItemList.jsx
@@ -7,12 +7,21 @@ const ItemList = ({items}) => {
     //console.log('Soy los producots', items)
     
     //cuando hago esto me aparece el error
-    items.map(item => console.log(item.title))  
+    items.map(item => console.log(item))  
 
   return (
     <div className='itemListContenedor'>
-     
-    
+      {items.map((item) => {
+        return (
+          <div key={item.id}>
+            <img src={item.img} alt=""/>
+            <h4>${item.title}</h4>
+            <p>${item.price}</p>
+
+          </div>
+        );
+      })}
+        
     </div>
   )
 }
