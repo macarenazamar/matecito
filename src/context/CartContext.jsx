b036f6132bb55aa054c1173d1930463fb9ba6e86
macarena_zamar <mmacarenazamar@gmial.com>
2022-09-25 15:19:54 +0200

    Corrigiendo funciones de context

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -7,31 +7,81 @@ export const CartContext = createContext ();
 const CartProvider = ({children}) => {
     //creo el estado
     const [cart, setCart] = useState ([]);
+
     //creo funcion que setea el estado
     const addToCart = (info, cantidad) => {
-        //console.log({...info, cantidad});
-        if (false) {
-            
+        if (isInCart(info.id)) {
+            //lo encuentro y le sumo la cantidad
+            totalQuantitySingleProduct(info, cantidad);
         } else {
             setCart([...cart, { ...info, cantidad }])
         }
     };
-    //funcion para vaciar todo el carrito
-    const clearCart = () => {
-        setCart([])
+
+    // corroborar si el producto ya está en el carrito (isInCart)
+     const isInCart = (id) => {
+        return cart.some((prod) => prod.id === id);
     };
-    console.log(cart)
-    //funciones que debo crear:
-    //corroborar si el producto ya esta en el carrito
-    //eliminar un solo producto
+
+    //sumar cantidad del mismo producto
+    const totalQuantitySingleProduct = (info, cantidad) => {
+        const updateProducts = cart.map((prod) => {
+            if (prod.id === info.id) {
+                const productUpdated = {
+                    ...prod,
+                    cantidad: cantidad,
+                };
+
+                return productUpdated;
+            } else {
+                return prod;
+            }
+        });
+        setCart(updateProducts);
+    };
+
     //calcular total de unidades para el cart widget
-    //calcular total precio
-    //sumar cantidades del mismo producto
-    //que los voy a pasar a traves del value 
+    const totalQuantity = () => {};
+
+    //calcular total precio del carrito
+    const totalPrice = () => {
+        let acumulador = 0;
+        cart.forEach((prod) => {
+            acumulador += prod.cantidad * prod.price;
+        });
+        return acumulador;
+    };
+
+    //eliminar un solo producto pasandole el id
+    const deleteOne = (id) => {
+
+        const filteredProducts = cart.filter((prod) => prod.id !== id);
+        setCart(filteredProducts);
+    };
+
+    //vaciar todo el carrito
+    const clearCart = () => {
+        setCart([]);
+    };
+
+    const getProductQuantity = (id) => {
+        const product = cart.find((prod) => prod.id === id);
+        return product?.cantidad;
+    };
+
     return (
-        <CartContext.Provider value={{cart, addToCart, clearCart}}>
+        <CartContext.Provider value={{
+            cart,
+            addToCart,
+            clearCart,
+            deleteOne,
+            totalPrice,
+            totalQuantity,
+            getProductQuantity,
+        }}
+        >
             {children}
         </CartContext.Provider>
-    )
-}
+    );
+};
 export default CartProvider
